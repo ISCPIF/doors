a91b3455f38df697181ed194db64410d07d29030
Romain Reuillon <romain.reuillon@iscpif.fr>
2017-02-09 12:55:29 +0100

    Compile after parsing.

--- application/server/src/main/scala/fr/iscpif/doors/server/Doors.scala
+++ application/server/src/main/scala/fr/iscpif/doors/server/Doors.scala
@@ -4,20 +4,20 @@ import better.files._
 
 object Doors extends App {
 
-  case class Config(settings: Option[Settings] = None)
+  case class Config(settings: Option[String] = None)
 
-  val parser = new scopt.OptionParser[Config]("scopt") {
+  val parser = new scopt.OptionParser[Config]("doors") {
     head("doors", "0.1")
 
     opt[String]('s', "settings").action { (x, c) =>
-      c.copy(settings = Some(Settings.compile(File(x).lines.mkString("\n"))))
+      c.copy(settings = Some(File(x).lines.mkString("\n")))
     }.text("the path to the settings files")
   }
 
   val config = parser.parse(args, Config()).get
 
   config.settings match {
-    case Some(s) => Launcher.run(s)
+    case Some(s) => Launcher.run(Settings.compile(s))
     case None =>
       println("Missing argument settings.")
       parser.showUsageAsError()
